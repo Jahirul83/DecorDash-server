28b17c9c1c1cab32fbccc4bbd620f22eada3a59a
Jahirul83 <jahirulislamcse83@gmail.com>
2024-06-08 17:02:10 +0600

    carts collection add

--- index.js
+++ index.js
@@ -14,12 +14,8 @@ app.use(express.json());
 
 
 
-// const uri = "mongodb+srv://<username>:<password>@cluster0.m2pwikj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m2pwikj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// console.log('DB_USER:', process.env.DB_USER);
-// console.log('DB_PASS:', process.env.DB_PASS);
+// const uri = "mongodb+srv://<username>:<password>@cluster0.52lko5e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.52lko5e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -35,13 +31,27 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-
         const productsCollection = client.db("DecorDb").collection("products");
+        const cartsCollection = client.db("DecorDb").collection("carts");
 
-        app.get('/products',async(req,res)=>{
+        app.get('/products', async (req, res) => {
             const result = await productsCollection.find().toArray();
             res.send(result);
         })
+
+        app.post('/carts', async (req, res) => {
+            const  allCarts = req.body;
+            const result = await cartsCollection.insertMany(allCarts);
+            res.send(result);
+
+            // console.log(allCarts);
+        })
+
+        app.get('/carts', async (req, res) => {
+            const result = await cartsCollection.find().toArray();
+            res.send(result);
+        })
+
 
 
 
@@ -54,6 +64,8 @@ async function run() {
     }
 }
 run().catch(console.dir);
+
+
 
 
 
